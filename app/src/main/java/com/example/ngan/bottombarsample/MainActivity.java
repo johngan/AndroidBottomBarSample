0a46de17a0e76087611fc36bc1f4538454ff0519
ngantoday <ngantoday@gmail.com>
2016-05-20 15:15:41 +0800

    first commit

---
+++ app/src/main/java/com/example/ngan/bottombarsample/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.ngan.bottombarsample;
+
+import android.support.annotation.IdRes;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomBar mBottomBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // hide BottomBar onScroll
+        mBottomBar = BottomBar.attachShy(
+                (CoordinatorLayout) findViewById(R.id.myCoordinator),
+                findViewById(R.id.myScrollingContent), savedInstanceState
+        );
+
+        mBottomBar.useFixedMode();
+        mBottomBar.setTextAppearance(R.style.MyTextAppearance);
+
+        mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+//                mMessageView.setText(getMessage(menuItemId, false));
+//                SampleFragment sampleFragment = SampleFragment.newInstance(getMessage(menuItemId, false));
+//
+//                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//                transaction.replace(R.id.fragment_container, sampleFragment);
+//                transaction.addToBackStack(null);
+//                transaction.commit();
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                Toast.makeText(getApplicationContext(), getMessage(menuItemId, true), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private String getMessage(int menuItemId, boolean isReselection) {
+        String message = "Content for ";
+
+        switch (menuItemId) {
+            case R.id.bb_menu_recents:
+                message += "recents";
+                break;
+            case R.id.bb_menu_favorites:
+                message += "favorites";
+                break;
+            case R.id.bb_menu_nearby:
+                message += "nearby";
+                break;
+            case R.id.bb_menu_friends:
+                message += "friends";
+                break;
+            case R.id.bb_menu_food:
+                message += "food";
+                break;
+        }
+
+        if (isReselection) {
+            message += " WAS RESELECTED! YAY!";
+        }
+
+        return message;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Necessary to restore the BottomBar's state, otherwise we would
+        // lose the current tab on orientation change.
+        mBottomBar.onSaveInstanceState(outState);
+    }
+}
